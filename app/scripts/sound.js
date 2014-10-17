3fcf3af46d900fb9dce5f042269f3f326afb5126
algm <piticonejo@gmail.com>
2014-10-17 22:50:20 +0200

    First commit

---
+++ app/scripts/sound.js
@@ -0,0 +1,47 @@
+;(function(window, $, undefined) {
+	"use strict";
+
+	function Sound() {
+		this.loaded = $.Deferred();
+		this.loadCount = 0;
+		this.total = 1;
+		this.sounds = {};
+	}
+
+	Sound.prototype.load = function() {
+		var _self = this, to;
+
+		function waitForLoad() {
+			if (_self.loadCount < _self.total) {
+				return setTimeout(waitForLoad, 50);
+			} else {
+				_self.loaded.resolve(_self);
+				clearTimeout(to);
+			}
+
+			return false;
+		}
+
+		to = waitForLoad();
+
+		this.sounds.shoot = new Howl({
+		  urls: ['res/sound/playershoot.mp3'],
+		  volume: 0.5,
+		  onload: function() {
+		  	_self.loadCount++;
+		  }
+		});
+
+		return this.loaded;
+	}
+
+	Sound.prototype.play = function(sound) {
+		if (typeof this.sounds[sound] !== 'undefined') {
+			this.sounds[sound].play();
+		}
+
+		return this;
+	}
+
+	window.Sound = Sound;
+})(window, jQuery);
